1b0db2a02186c4dc04320df512e6a7d4b5e505ba
Nafisha <51881741+Nafisha-del@users.noreply.github.com>
2026-05-07 10:08:00 +0600

    Update Advisor.java
    Add one-to-many relationship between Advisor and Clients

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,16 +1,14 @@
 package com.wellsfargo.counselor.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 public class Advisor {
 
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long advisorId;
 
     @Column(nullable = false)
@@ -28,16 +26,20 @@ public class Advisor {
     @Column(nullable = false)
     private String email;
 
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
+
     protected Advisor() {
 
     }
 
-    public Advisor(String firstName, String lastName, String address, String phone, String email) {
+    public Advisor(String firstName, String lastName, String address, String phone, String email, List<Client> clients) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.phone = phone;
         this.email = email;
+        this.clients = clients;
     }
 
     public Long getAdvisorId() {
@@ -82,5 +84,13 @@ public class Advisor {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
     }
 }
